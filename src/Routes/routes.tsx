1191ca347754c4fe861801eb68d2e1545ca95097
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-11-30 19:52:31 -0300

    :sparkles: feat: Add Home to Routes

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        children: []
+        children: [
+            {path: "/", element: <Home />}
+        ]
     }
 ])
