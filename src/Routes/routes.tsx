0232de103ad63edc150f10b6e97bf69da19e2aee
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-12-01 10:45:39 -0300

    :sparkles: feat: Add register and login routes

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            {path: "/", element: <Home />}
+            {path: "/", element: <Home />},
+            {path: "/register", element: <Register />},
+            {path: "/login", element: <Login />},
         ]
     }
 ])
