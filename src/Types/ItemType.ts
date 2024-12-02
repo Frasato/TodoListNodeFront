51150338fa2cf2ec6d75eff4898d1b6ea28a2042
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-12-02 08:55:02 -0300

    :sparkles: feat: Cahnge Event type

--- src/Types/ItemType.ts
+++ src/Types/ItemType.ts
@@ -1,4 +1,8 @@
+import { ChangeEvent } from "react"
+
 export type ItemType = {
     title: string,
     description: string,
+    status: boolean,
+    statusChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
